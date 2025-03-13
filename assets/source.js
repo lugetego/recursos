3a37b05c4f32834cfea623c56879486e0f115da3
Luis Gerardo Tejero <lugetego@hotmail.com>
2025-03-12 19:28:34 -0600

    Se agrega campo tutor

--- assets/source.js
+++ assets/source.js
@@ -13,6 +13,7 @@ fuente.addEventListener("change", () => {
         responsable.style.display = "none";
         labelResponsable.style.display = "none";
         responsable.removeAttribute("required"); // Quitar required cuando está oculto
+        responsable.value="";
     } else {
         responsable.style.display = "block";
         labelResponsable.style.display = "block";
